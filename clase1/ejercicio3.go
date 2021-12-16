72b7677127faf4862bca5b4537b5690952cd95dd
Daniel Chacon <daniel.chaconcardenas@mercadolibre.com>
2021-12-16 11:46:27 -0300

    class 1 finalized

--- clase1/ejercicio3.go
+++ clase1/ejercicio3.go
@@ -1,17 +1,11 @@
 var 1nombre string MAL
 solucion var nombre string 
-
 var apellido string GOOD
-
 var int edad MAL
 solucion var edad int 
-
 1apellido := 6 MAL
 solucion apellido:= 6
-
 var licencia_de_conducir = true GOOD
-
 var estatura de la persona int MAL
 solucion var estaturaDeLaPersona int
-
 cantidadDeHijos := 2 GOOD
--- clase1/ejercicio4.go
+++ clase1/ejercicio4.go
@@ -1,6 +1,8 @@
 var apellido String = "Gomez" GOOD 
 var edad int ="35" MAL
 solucion var edad int = 35
+boolean :="false" MAL
+solucion boolean:=false
 var sueldo string = 45857.90 MAL
 solucion var sueldo float = 45857.90
 var nombre string = "Julian" good
