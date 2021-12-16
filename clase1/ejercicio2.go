6ae832054030c65b0b833512fb02ef2d8c3b4f67
Daniel Chacon <daniel.chaconcardenas@mercadolibre.com>
2021-12-16 11:40:00 -0300

    class 1 completed

---
+++ clase1/ejercicio2.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+func main(){
+	var(
+		temperature = 34
+		humedad = "60%"
+		presion = 10.2
+	)
+	fmt.Println("Temperature:",temperature)
+	fmt.Println("Humedad", humedad)
+	fmt.Println("Presion:",presion)
+}
